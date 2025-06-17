277f9aa388a5a3694899afe7b08ef7565707d401
dangkhoa1102 <dangkhoa.dni@gmail.com>
2025-06-17 13:16:12 +0700

    Add CheckBox_Su_Kien WPF application to solution
    Updated PRN212-SE1848.sln to include a new WPF application
    named CheckBox_Su_Kien, along with its project files and
    configurations. Created App.xaml and App.xaml.cs for the
    application entry point and resources. Added
    AssemblyInfo.cs for WPF theme information. Defined the
    user interface in MainWindow.xaml with checkboxes for
    user preferences, and implemented event handling logic
    in MainWindow.xaml.cs to manage checkbox interactions.

---
+++ CheckBox_Su_Kien/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckBox_Su_Kien
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void chkToanBo_Checked(object sender, RoutedEventArgs e)
+        {
+            chkCongNghe.IsChecked = true;
+            chkDuLich.IsChecked = true;
+            chkDaBong.IsChecked = true;
+            chkXemPhim.IsChecked = true;
+        }
+
+        private void chkToanBo_Unchecked(object sender, RoutedEventArgs e)
+        {
+            chkCongNghe.IsChecked = false;
+            chkDuLich.IsChecked = false;
+            chkDaBong.IsChecked = false;
+            chkXemPhim.IsChecked = false;
+        }
+
+        private void chkCongNghe_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chkDuLich.IsChecked == true && chkDaBong.IsChecked == true && chkXemPhim.IsChecked == true)
+                chkToanBo.IsChecked = true;
+        }
+
+        private void chkCongNghe_Unchecked(object sender, RoutedEventArgs e)
+        {
+            chkToanBo.IsChecked = false;
+        }
+
+        private void chkDuLich_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chkCongNghe.IsChecked == true && chkDaBong.IsChecked == true && chkXemPhim.IsChecked == true)
+                chkToanBo.IsChecked = true;
+        }
+
+        private void chkDuLich_Unchecked(object sender, RoutedEventArgs e)
+        {
+            chkToanBo.IsChecked = false;
+        }
+
+        private void chkDaBong_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chkCongNghe.IsChecked == true && chkDuLich.IsChecked == true && chkXemPhim.IsChecked == true)
+                chkToanBo.IsChecked = true;
+        }
+
+        private void chkDaBong_Unchecked(object sender, RoutedEventArgs e)
+        {
+            chkToanBo.IsChecked = false;
+        }
+
+        private void chkXemPhim_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chkCongNghe.IsChecked == true && chkDuLich.IsChecked == true && chkDaBong.IsChecked == true)
+                chkToanBo.IsChecked = true;
+        }
+
+        private void chkXemPhim_Unchecked(object sender, RoutedEventArgs e)
+        {
+            chkToanBo.IsChecked = false;
+        }
+    }
+}
