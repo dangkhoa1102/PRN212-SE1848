31ae4871b6699bdc6cdee774bccd48952564404f
dangkhoa1102 <dangkhoa.dni@gmail.com>
2025-07-01 14:35:51 +0700

    hej

---
+++ MyStore_EntityFrameWork/AdminWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using MyStore_EntityFrameWork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MyStore_EntityFrameWork
+{
+    /// <summary>
+    /// Interaction logic for AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        MyStoreContext context = new MyStoreContext();
+        public AdminWindow()
+        {
+            InitializeComponent();
+            LoadCategoriesIntoTreeView();
+        }
+
+        private void LoadCategoriesIntoTreeView()
+        {
+            //Tạo gốc cây
+            TreeViewItem root = new TreeViewItem();
+            root.Header = "Kho hàng Cát Lái";
+            tvCategory.Items.Add(root);
+            //Truy vấn toàn bộ danh mục
+            var categories = context.Categories.ToList();
+            //Đưa danh mục lên treeview
+            foreach (var category in categories)
+            {
+                TreeViewItem category_node = new TreeViewItem();
+                category_node.Header = category.CategoryName;
+                category_node.Tag = category;
+                root.Items.Add(category_node);
+                var products = context.Products
+                    .Where(x => x.CategoryId == category.CategoryId)
+                    .ToList();
+                //Nap product vao node danh muc tuong ung
+                foreach (var product in products)
+                {
+                    TreeViewItem product_node = new TreeViewItem();
+                    product_node.Header = product.ProductName;
+                    product_node.Tag = product;
+                    category_node.Items.Add(product_node); // Changed from root.Items to category_node.Items
+                }
+            }
+        }
+
+        private void tvCategory_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (e.NewValue == null) return;
+            TreeViewItem item = e.NewValue as TreeViewItem;
+            if(item == null) return;
+            Category category = item.Tag as Category;
+            if(category == null) return;
+            LoadProductsIntoListView(category);
+        }
+
+        private void LoadProductsIntoListView(Category category)
+        {
+            var products = context.Products
+                    .Where(x => x.CategoryId == category.CategoryId)
+                    .ToList();
+            lvProduct.ItemsSource = null;
+            lvProduct.ItemsSource = products;
+        }
+    }
+}
---
+++ MyStore_EntityFrameWork/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using MyStore_EntityFrameWork.Models;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyStore_EntityFrameWork
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MyStoreContext context = new MyStoreContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ChangeBackground();
+        }
+        private void ChangeBackground()
+        {
+            LinearGradientBrush brush = new LinearGradientBrush();
+            brush.GradientStops.Add(new GradientStop(Colors.Red, 0.0));
+            brush.GradientStops.Add(new GradientStop(Colors.Purple, 1.0));
+            brush.GradientStops.Add(new GradientStop(Colors.RosyBrown, 0.5));
+            btnThoat.Background = brush;
+            //Đổi màu nền nút đăng nhập
+            RadialGradientBrush brush2 = new RadialGradientBrush();
+            brush2.GradientStops.Add(new GradientStop(Colors.Green, 0.0));
+            brush2.GradientStops.Add(new GradientStop(Colors.LightCyan, 1.0));
+            brush2.GradientStops.Add(new GradientStop(Colors.LightCoral, 0.5));
+            btnDangNhap.Background = brush2;
+        }
+
+        private void btnThoat_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnDangNhap_Click(object sender, RoutedEventArgs e)
+        {
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Password.Trim();
+            AccountMember account = context.AccountMembers.FirstOrDefault(a => a.EmailAddress == email && a.MemberPassword == password);
+            if (account == null)
+            {
+                MessageBox.Show("Email hoặc mật khẩu không đúng!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Error);
+                
+                return;
+            }
+            else
+            {
+                if(account.MemberRole == 1)
+                {
+                    MessageBox.Show("Đăng nhập Admin Thành Công","Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                    AdminWindow adminWindow = new AdminWindow();
+                    adminWindow.Show();
+                    Close();
+                    return;
+                }
+                else if (account.MemberRole == 2)
+                {
+                    MessageBox.Show("Đăng nhập Staff Thành Công", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                    StaffWindow staffWindow= new StaffWindow();
+                    staffWindow.Show();
+                    return;
+                }
+                else if (account.MemberRole == 3)
+                {
+                    MessageBox.Show("Đăng nhập Member Thành Công", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                    MemberWindow memberWindow = new MemberWindow();
+                    memberWindow.Show();
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Đăng nhập không thành công!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+    }}
+}
